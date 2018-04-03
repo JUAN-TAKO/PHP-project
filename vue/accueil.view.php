347b02192296fc3a700e8e654da55f8b08eb70f4
RAKOTOARISOA Harena <rakotoah@hostname.iut2.upmf-grenoble.fr>
2018-04-03 08:58:59 +0200

    Ourtube

--- vue/accueil.view.php
+++ vue/accueil.view.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PasYoutube</title>
+    <title>Ourtube</title>
 </head>
-<h1>PasYoutube</h1>
+<h1>Ourtube</h1>
 <body>
     
 </body>
